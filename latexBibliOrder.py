dbdfda0923ede3fe66af7ad19df9b2a9a71ae28b
Marie Combe <marie.combe@wur.nl>
2016-02-10 08:48:22 +0100

    updating the pseudocode according to our last discussion and adding and example bib file to clean up

--- latexBibliOrder.py
+++ latexBibliOrder.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 """
-This program inputs a text (.rtf) file, finds its bibtex entries
-and reorders its items in a designated order that DFB likes.
+This program reads one or more bib file, separates its bibtex entries
+and reorders its items in a designated order.
 
 Processing include:
     -- reordering
@@ -13,7 +13,7 @@ Processing include:
     -- save file as latexBibliOrder.bib
 """
 
-import sys, os, glob, re
+import sys, os, glob
 import numpy as np
 
 
@@ -22,111 +22,110 @@ def main():
 #===============================================================================
 
     """
-    The main() function does the main part of script
+    This main() function does the main part of script
 
     """
 
-    # define the folder to search .bib files in (here: I assume the current dir)
-    cwd = os.getcwd()
+    # make python get the fullpath to the current working directory
+    # and define useful folder names
+    cwd           = os.getcwd()
+    input_folder  = os.path.join(cwd, 'bib_input_files/')
+    output_folder = os.path.join(cwd, 'bib_output_file/')
 
-    # list the input .bib files to read: it is actually easy in two lines:
-    # first define the pattern you are looking for in the current directory:
-    pattern = os.path.join(cwd, “*.bib”)
-    # then create a list of all files that have the “.bib” name extension
-    inFileList = glob.glob(pattern) 
+    # list the input .bib files to read in the input folder (two steps)
+    # a- define the pattern you are looking for:
+    pattern       = os.path.join(input_folder, “*.bib”)
+    # b- create a list of all files that fullfil this pattern
+    inFileList    = glob.glob(pattern) 
 
-    # if we do not find a “.bib” file in the specified folder, we exit
+    # Troubleshooting: if we do not find a “.bib” file in the input folder:
+    # a- we warn the user
+    # b- we exit the script
     if len(inFileList==0):
-        print “there are no ‘.bib’ files in folder %s”%cwd
+        print 'there are no .bib files in folder %s'%cwd
         sys.exit()
 
-    # else, we proceed with the cleaning code:
+    # else, we proceed with cleaning the input files:
     else:
 
         # ------------------ here the actual cleaning begins -------------------
         # create an empty output “biblio.bib” file
+        ....
 
         # initialize an empty list to store clean bib_items from all files
+        list_bib_items = list()
 
         # ------------------------ loop over .bib files ------------------------
-        # for each refs.bib file: 
+
+        # for each input file: 
         for ...:
 
-            # read the content of the file as one string
+            # read the content of the file as one string then close the file
             f = open(filename, ‘r’) # r for read-only
-            one_string = f.read()
+            file_as_one_string = f.read()
             f.close()
 
             # split the contents into bib_item:
             bib_separator = ‘@’
-            list_bib_items = file_as_one_string.split(bib_separator)
+            bib_items     = file_as_one_string.split(bib_separator)
 
-            # ----------------- loop over unclean bib entries ------------------
-            # for each unclean bib_item:
-            for ...:
-
-                # split the bib_item into its ref_item (cite_key, title, ...)
-                ref_separator = ‘\n’
-                list_ref_items = bib_item.split(ref_separator)
-
-                # clean all ref_items of the bib_item
-                clean_bib_item = clean_bib(list_ref_items)
-
-                # add clean_bib_item to the list of clean bib_items
-
-            # ---------------- we exit the loop on bib entries -----------------
+            # append the bib items of this file to the list of all bib items
+            # from all files
+            list_bib_items += bib_items
 
         # ------------------- we exit the loop on bib files --------------------
 
-        # within the final list of clean_bib_items, compare unique cite_keys and
-        # remove duplicates
+        # ----------------- loop over unclean bib entries ----------------------
 
-        # if len(clean_bib_items)==0
-            # sort the list of clean bib_items alphabetically 
+        # we initialize a matrix in which to store the product of our cleaning
+        # number of columns = number of bib entries
+        # number of rows    = number of ref items per entry
+        nbcol  = ...
+        nbrows = ...
+        clean_entries = np.zeros((nbcol, nbrows))
+
+        # for each bib_item of the total list:
+        for column in len():
+
+            # split the bib_item into its ref_item (cite_key, title, ...)
+            ref_separator = ‘\n’
+            list_ref_items = bib_item.split(ref_separator)
+
+            # clean the ref_items and store them into out matrix
+            clean_entries[column,:] = clean_bib(list_ref_items)
+
+        # ---------------- we exit the loop on bib entries ---------------------
+
+        # Finally: remove duplicates from matrix
+
+        # sort alphabetically 
+
+        # initialize an output file, which will be stored in the 'output' folder
 
         # write all non-duplicates, sorted clean bib_items to the master.bib file:
         # NB: what you write is entry_type dependant
-        if entry_type=='article':
-             write (....)
-        elif entry_type=='book': 
-             write (....)
-        elif entry_type=='chapter':
-             write (....)
-        elif entry_type=='website':
-             write (....)
-        else:
-             write (....)
 
+        'for all clean bib items:'
+
+            if entry_type=='article':
+                 output_file.write (....)
+            elif entry_type=='book': 
+                 output_file.write (....)
+            elif entry_type=='chapter':
+                 output_file.write (....)
+            elif entry_type=='website':
+                 output_file.write (....)
+            else:
+                 output_file.write (....)
+
+        # close the output file
+
+    # write a message to user to report success of the cleaning, and where to
+    # find the clean file
+
+    # exit the script
     return None
 
-"""
-Some possible features to add to the main script:
--------------------------------------------------
-
-I don’t know how difficult would this be, but… what about warning if it finds
-entries that have largely coincidental strings?, i.e., duplicates but that have
-mistakes on them, that a super thorough examination (one to one) won’t detect.
-
-I think this is too difficult to automate if we have done the cleaning to the
-max. At the end, the user will anyway look at the sorted bibliography to get
-the cite_keys for LaTex, and he/she can detect “hidden” duplicates and delete
-them (they will follow each other in the clean list, since such duplicates
-should have almost same authors, year, title)
-
-
-Define what an individual bibtex entry is:
-    bib_item = []
-    key_string = "@"
-    separator = re.findall(r'{@}',exampleString)
-
-def bib_item():
-    for key_string in bib_file:
-        if key_string()
-
-Find everthing between @, and include each in bib_item including @ in the first
-one, and leaving out the second one
-
-"""
 
 
 #===============================================================================
@@ -206,3 +205,32 @@ def clean_bib(list_ref_items):
 if __name__=='__main__':
     main()
 #===============================================================================
+
+"""
+Some possible features to add to the main script:
+-------------------------------------------------
+
+I don’t know how difficult would this be, but… what about warning if it finds
+entries that have largely coincidental strings?, i.e., duplicates but that have
+mistakes on them, that a super thorough examination (one to one) won’t detect.
+
+I think this is too difficult to automate if we have done the cleaning to the
+max. At the end, the user will anyway look at the sorted bibliography to get
+the cite_keys for LaTex, and he/she can detect “hidden” duplicates and delete
+them (they will follow each other in the clean list, since such duplicates
+should have almost same authors, year, title)
+
+
+Define what an individual bibtex entry is:
+    bib_item = []
+    key_string = "@"
+    separator = re.findall(r'{@}',exampleString)
+
+def bib_item():
+    for key_string in bib_file:
+        if key_string()
+
+Find everthing between @, and include each in bib_item including @ in the first
+one, and leaving out the second one
+
+"""
